cc942faac9e952246e4354d12f12968e9c4ecb57
DavidDark <link100david@hotmail.com>
2018-02-02 17:03:02 -0600

    Practica 1 x David Israel Perez Montes

---
+++ ene-jun-2018/prácticas/DavidIsraelPerezMontes/práctica1/practica1.go
@@ -0,0 +1,30 @@
+package main
+import (
+	"fmt"
+	"time"
+        "os"
+        "bufio"
+)
+
+const End = 100 * time.Millisecond
+
+func hello(i int) {
+	time.Sleep(10 * time.Duration(i%5) * time.Millisecond)
+	fmt.Println("Hola")
+}
+func palabras(i int,text string) {
+        time.Sleep(10 * time.Duration(i%5) * time.Millisecond)
+        fmt.Print(text)
+}
+
+func main() {
+ reader := bufio.NewReader(os.Stdin)
+    fmt.Print("Enter text: ")
+    text, _ := reader.ReadString('\n')
+
+	for i := 1; i <= 10; i++ {
+		go hello(i)
+               go palabras(i,text)
+	}
+	time.Sleep(End)
+}
