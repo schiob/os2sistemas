35f4c91542f4df6ef5372d123db61853d169ad75
JCsleiman <jcsleiman17@gmail.com>
2018-02-02 21:49:12 -0600

    Practica 1: Juan Carlos Sleiman

--- ene-jun-2018/Juan Carlos Sleiman/Practica 1/hola.go
+++ ene-jun-2018/Juan Carlos Sleiman/Practica 1/hola.go
@@ -1,26 +1,21 @@
  package main
 
 import (
-"bufio"
 "fmt"
-"os"
 "time"
 )
-
-var text string
 
 func say(s string) {
 for i := 0; i < 10; i++ {
 time.Sleep(100 * time.Millisecond)
 fmt.Println(s)
-fmt.Println(text)
 }
 }
 
 func main() {
-reader := bufio.NewReader(os.Stdin)
-fmt.Print("Enter text: ")
-text, _ := reader.ReadString('\n')
+var text string
+fmt.Print()
+fmt.Scan(&text)
 go say("hola")
 say(text)
 }
