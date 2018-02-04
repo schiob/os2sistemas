71f4d02d44e789350cabeac10e8d242e8681fe83
JCsleiman <jcsleiman17@gmail.com>
2018-02-03 22:08:39 -0600

    Practica 1: Juan Carlos Sleiman (formatted)

--- ene-jun-2018/Juan Carlos Sleiman/Practica 1/hola.go
+++ ene-jun-2018/Juan Carlos Sleiman/Practica 1/hola.go
@@ -1,21 +1,21 @@
- package main
+package main
 
 import (
-"fmt"
-"time"
+	"fmt"
+	"time"
 )
 
 func say(s string) {
-for i := 0; i < 10; i++ {
-time.Sleep(100 * time.Millisecond)
-fmt.Println(s)
-}
+	for i := 0; i < 10; i++ {
+		time.Sleep(100 * time.Millisecond)
+		fmt.Println(s)
+	}
 }
 
 func main() {
-var text string
-fmt.Print()
-fmt.Scan(&text)
-go say("hola")
-say(text)
+	var text string
+	fmt.Print()
+	fmt.Scan(&text)
+	go say("hola")
+	say(text)
 }
